af580709d6f3aaddde5601bacdb392fed2dd5a10
bensmus <37351071+bensmus@users.noreply.github.com>
2019-03-14 10:53:01 -0700

    Update team-guardian-contour-detection.py

--- team-guardian-contour-detection.py
+++ team-guardian-contour-detection.py
@@ -2,40 +2,39 @@ import numpy as np
 import cv2 as cv
 
 im = cv.imread('emitter.png')
+
+# using this additional blur
+# provides more reliable results with other images
 im = cv.medianBlur(im, 5)
 
-# second argument sorts edges by length?
+# the second threshold eliminates the shorter edges
 edges = cv.Canny(im, 0, 200)
 
-# make the edges that are close to each other closed
-# this is so that blob detection is easier
+# merge the edges that are close to each other 
 kernel = np.ones((10, 10), np.uint8)
 edgesclosed = cv.morphologyEx(edges, cv.MORPH_CLOSE, kernel)
 
 contours, hierarchy = cv.findContours(edgesclosed, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
 for i, contour in enumerate(contours):
-    '''
-    x,y,w,h = cv.boundingRect(contour)
-    aspect_ratio = float(w)/h
-    print(aspect_ratio)
-    '''
-    # solidity works well for recognizing blobs
+    
+    # solidity works well for recognizing circle-like shapes
     area = cv.contourArea(contour)
     hull = cv.convexHull(contour)
     hullArea = cv.contourArea(hull)
-    solidity = area / float(hullArea)
-    print(solidity)
+    
+    # eliminate division by zero bugs
+    if hullArea != 0:
+        solidity = area / float(hullArea)
 
-    if solidity > 0.8:
-        #cv.drawContours(im, contours[i], -1, (0, 0, 255), 3)
-        (x,y),radius = cv.minEnclosingCircle(contour)
-        center = (int(x),int(y))
-        print(center)
-        radius = int(radius)
-        cv.circle(im, center, radius,(0, 0, 255), 2)
-
-print('number of contours', len(contours))
+        if solidity > 0.8:
+            (x,y), radius = cv.minEnclosingCircle(contour)
+            center = (int(x), int(y))
+            radius = int(radius)
+            cv.circle(im, center, radius,(0, 0, 255), 2)
+            
+            # the final result: coordinates of the blob
+            print(center)
 
 cv.imshow('emitter with contours', im)
 cv.waitKey(0)
